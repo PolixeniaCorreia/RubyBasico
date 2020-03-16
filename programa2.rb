4403e872491d65b23a4fc77525c285716f50dbb6
Polixenia Correia <polixeniacorreia@gmail.com>
2020-03-16 17:36:39 -0300

    Operadores

---
+++ programa2.rb
@@ -0,0 +1,15 @@
+a,b = 1,2
+puts a
+puts b
+
+if a > b
+    puts "a é maior que b."
+else
+    puts "b é maior que a."
+end
+
+puts "a é menor que b." if a < b
+
+puts 1 + 1
+puts 10 % 5
+puts 2 ** 3
