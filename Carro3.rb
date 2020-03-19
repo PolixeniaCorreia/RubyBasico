907398784defdf4f49a45589e3d325528879f89e
Polixenia Correia <polixeniacorreia@gmail.com>
2020-03-18 21:48:00 -0300

    Abrindo Classes

---
+++ Carro3.rb
@@ -0,0 +1,16 @@
+class Carro
+
+    def acelera
+        puts "Acelerando..."
+    end
+end
+class Carro
+
+    def freia
+        puts "Parando..."
+    end
+end
+
+carro = Carro.new
+carro.acelera
+carro.freia
