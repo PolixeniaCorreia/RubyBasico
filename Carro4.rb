650e16f09ca07a7088754e8bc11aa3dcbcc4a5f1
Polixenia Correia <polixeniacorreia@gmail.com>
2020-03-18 21:53:03 -0300

    Monkey Patching

---
+++ Carro4.rb
@@ -0,0 +1,14 @@
+class Carro
+    def acelera
+        puts "Acelerando"
+    end
+end
+
+class Carro
+    def acelera
+        puts "Novo Método Acelera"
+    end
+end
+
+carro = Carro.new
+carro.acelera
