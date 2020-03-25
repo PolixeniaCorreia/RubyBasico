584b03c33c86c29f35f51a8484e1dd71759d2020
Polixenia Correia <polixeniacorreia@gmail.com>
2020-03-25 20:28:22 -0300

    Strings

---
+++ Strings.rb
@@ -0,0 +1,18 @@
+marca = 'Jeep'
+modelo = "renegade"
+motor = 2.0
+
+frase = "#{marca} #{modelo} tem motor #{motor}"
+
+puts marca
+puts modelo
+puts motor
+puts frase
+
+puts modelo.encoding
+
+puts "'aspas simples' \"aspas dupla\" "
+
+
+
+
