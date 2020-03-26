d662deed8369b8db2717036ae8c9b35d041a5cce
Polixenia Correia <polixeniacorreia@gmail.com>
2020-03-25 21:06:48 -0300

    Operadores em Strings

---
+++ OpStrings.rb
@@ -0,0 +1,19 @@
+var = "Poli"
+var2 = "Poli"
+var3 = "  muito      espaço"
+
+puts var.downcase
+puts var2.upcase
+puts var3.strip
+
+nome = "Poli Correia"
+puts nome
+nome["Poli"] = "Polixenia"
+
+puts nome
+
+carro = "jeep renegade"
+puts carro.capitalize
+puts carro.chars
+puts carro.split
+puts carro.split("e")
