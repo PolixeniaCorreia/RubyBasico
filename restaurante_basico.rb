9d96bf75dade1381e5ef535016a86885a94e045a
Polixenia Correia <polixeniacorreia@gmail.com>
2020-03-30 15:29:39 -0300

    Interpolação de Strings

--- restaurante_basico.rb
+++ restaurante_basico.rb
@@ -3,6 +3,10 @@
 # print "Nome do restaurante: "
 # puts nome
 
-mensagem = "Bom dia, "
-mensagem << "tudo bem?"
-puts mensagem
+# mensagem = "Bom dia, "
+# mensagem << "tudo bem?"
+# puts mensagem
+
+puts "Digite o nome do restaurante"
+nome = gets
+print "Nome do restaurante: #{nome}"
