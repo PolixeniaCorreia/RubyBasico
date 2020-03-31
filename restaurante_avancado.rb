080ee44bfbe0a401af3acaaf3f441655e12a7de6
Polixenia Correia <polixeniacorreia@gmail.com>
2020-03-30 21:42:18 -0300

    Métodos parâmetros e parâmetros padrão.

---
+++ restaurante_avancado.rb
@@ -0,0 +1,12 @@
+def qualifica(nota)
+    puts "nota do restaurante foi #{nota}."
+end
+
+qualifica(10)
+
+def qualificar(nota, msg="Obrigada")
+    puts "A nota que qualifica restaurante foi #{nota}."
+end
+
+qualificar(10)
+qualificar(1,"Comida ruim.")
