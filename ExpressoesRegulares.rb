5b9c16c2f07891ee50985305bb50c42ca3748646
Polixenia Correia <polixeniacorreia@gmail.com>
2020-03-30 21:04:26 -0300

    Operador (ou igual)

--- ExpressoesRegulares.rb
+++ ExpressoesRegulares.rb
@@ -3,3 +3,10 @@ puts /paulo/ =~ "são paulo"
 puts /ulo/ =~ "são paulo"
 puts /são/ =~ "são paulo"
 puts /ão/ =~ "são paulo"
+
+nome ||= "anonimo"
+puts nome
+
+sobrenome = "Santos"
+sobrenome ||= "anonimo"
+puts sobrenome
